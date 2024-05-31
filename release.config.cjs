8542e8ebc5699d2540c00d1d5d40e249f4f20290
BITECHCO\namha <nam.work1909@gmail.com>
2024-05-31 14:47:31 +0700

    Config github actions

--- release.config.cjs
+++ release.config.cjs
@@ -10,12 +10,12 @@ module.exports = {
     '@semantic-release/changelog', // Updates the CHANGELOG.md file
     '@semantic-release/npm', // If you're publishing to npm, this updates the version in package.json and publishes the package
     '@semantic-release/github', // Creates a GitHub release
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
-        message: 'chore(release): :bookmark: bump version ${nextRelease.version}',
-      },
-    ],
+    // [
+    //   '@semantic-release/git',
+    //   {
+    //     assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+    //     message: 'chore(release): :bookmark: bump version ${nextRelease.version}',
+    //   },
+    // ],
   ],
 };
